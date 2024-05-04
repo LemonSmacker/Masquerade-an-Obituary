4a8a20e1d460c719f8a52c15e9084a524eeef424
LemonSmacker <123703586+LemonSmacker@users.noreply.github.com>
2024-05-03 20:24:34 -0700

    sound, respawn

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -9,6 +9,7 @@ const SENSITIVITY = 0.003
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
+var respawn_coords = Vector3(0, 0, 0)
 
 
 
@@ -17,11 +18,13 @@ var gravity = 9.8
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var jumpSound = $jumpsound
+@onready var stepSound = $stepsound
 
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	$RayCast3D.force_raycast_update()
 
 
 func _unhandled_input(event):
@@ -34,10 +37,12 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		stepSound.stop()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpSound.play()
 	
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
@@ -55,18 +60,36 @@ func _physics_process(delta):
 		if direction:
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
+			
 		else:
 			velocity.x = lerp(velocity.x, direction.x * speed, delta * 8.0)
 			velocity.z = lerp(velocity.z, direction.z * speed, delta * 8.0)
+			stepSound.play()
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.5)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.5)
+		
 	
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 	
 	
 	move_and_slide()
+	
+	
+	if position.y < -5:
+		position = respawn_coords
+	
+	
+	if $RayCast3D.is_colliding():
+		var collider = $RayCast3D.get_collider()
+		if collider.name == "CSGBox3D13":
+			respawn_coords = Vector3(-14.51, 15.122, 6.105)
+			print("Yippee")
+		if collider.name == "CSGBox3D25":
+			respawn_coords = Vector3(-50.52, 10.328, 13.04)
+			print("Yippee")
+	
 
 
 func _headbob(time) -> Vector3:
@@ -74,4 +97,5 @@ func _headbob(time) -> Vector3:
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	return pos
+
 
